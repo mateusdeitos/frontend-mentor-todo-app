228b9dd545fa5157d3a08f80b4802a35f4b45534
Mateus Deitos <mate.deitos@hotmail.com>
2023-08-17 16:17:06 -0300

    feat: preparing to build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/frontend-mentor-todo-appo/',
+	base: '/frontend-mentor-todo-app/',
 	plugins: [react()],
 	css: {
 		modules: {
